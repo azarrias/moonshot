116f208b8900053a1c14ffdbcbd716033622be7d
azarrias <adolfo.zarrias@gmail.com>
2020-11-13 13:35:30 +0100

    Update sprite batch for infinite scrolling sky layer

--- src/ScenePlay.lua
+++ src/ScenePlay.lua
@@ -2,28 +2,25 @@ ScenePlay = Class{__includes=tiny.Scene}
 
 function ScenePlay:init()
   self.player = self:CreatePlayer()
-  self.cameraOffsetX = 0
-  self.cameraSpeedX = 15
   self.sky = SkyLayer()
 end
 
 function ScenePlay:update(dt)
-  self.cameraOffsetX = self.cameraOffsetX + self.cameraSpeedX * dt
   self.sky:update(dt)
   self.player:update(dt)
 end
 
 function ScenePlay:render()
-  love.graphics.push()
-  love.graphics.translate(math.floor(-self.cameraOffsetX + 0.5), 0)
   self.sky:render()
-  love.graphics.pop()
   self.player:render()
+
+  love.graphics.setFont(FONTS['retroville-s'])
+  love.graphics.printf("cameraOffsetX: " .. math.floor(self.sky.cameraOffsetX), 5, 5, VIRTUAL_SIZE.x - 5, 'left')
 end
 
 function ScenePlay:CreatePlayer()
   local player = tiny.Entity(math.floor(VIRTUAL_SIZE.x / 2), math.floor(VIRTUAL_SIZE.y / 2))
-  local defaultQuadId = 2
+  local defaultQuadId = 1
   
   -- sprite component
   local playerSprite = tiny.Sprite(TEXTURES['player'], QUADS['player'][defaultQuadId])
--- src/SkyLayer.lua
+++ src/SkyLayer.lua
@@ -10,29 +10,46 @@ function SkyLayer:init()
   love.graphics.setCanvas()
   self.starImage = love.graphics.newImage(canvas:newImageData())
   
-  self.stars = {}
   self.spriteBatch = love.graphics.newSpriteBatch(self.starImage)
-  for i = 1, 100 do
-    self:AddStar(math.random(VIRTUAL_SIZE.x * 2), math.random(VIRTUAL_SIZE.y))
-  end
-  self:UpdateSpriteBatch()
+  self.startX = 1
+
+  self:ExtendSpriteBatch(self.startX)
+  self:ExtendSpriteBatch(self.startX)
+  
+  self.cameraOffsetX = 0
+  self.cameraSpeedX = 15
 end
 
 function SkyLayer:AddStar(x, y)
   table.insert(self.stars, { x = x, y = y })
 end
 
-function SkyLayer:UpdateSpriteBatch()
-  self.spriteBatch:clear()
-  for k, star in ipairs(self.stars) do
-    self.spriteBatch:add(star.x, star.y)
+function SkyLayer:ExtendSpriteBatch(startX)
+  for i = 1, 25 do
+    self.spriteBatch:add(math.random(startX, startX + VIRTUAL_SIZE.x), math.random(VIRTUAL_SIZE.y))
+  end
+  self.startX = self.startX + VIRTUAL_SIZE.x
+end
+
+function SkyLayer:UpdateSpriteBatch(startX)
+  local id = (startX - 1) / VIRTUAL_SIZE.x % 2
+  for i = id * 25 + 1, id * 25 + 25 do
+    self.spriteBatch:set(i, math.random(startX, startX + VIRTUAL_SIZE.x), math.random(VIRTUAL_SIZE.y))
   end
   self.spriteBatch:flush()
+  self.startX = self.startX + VIRTUAL_SIZE.x
 end
 
 function SkyLayer:update(dt)
+  self.cameraOffsetX = self.cameraOffsetX + self.cameraSpeedX * dt
+  if self.cameraOffsetX >= self.startX - VIRTUAL_SIZE.x then
+    self:UpdateSpriteBatch(self.startX)
+  end
 end
 
 function SkyLayer:render()
+  love.graphics.push()
+  love.graphics.translate(math.floor(-self.cameraOffsetX + 0.5), 0)
   love.graphics.draw(self.spriteBatch)
+  love.graphics.pop()
 end
