5d1d56bc1b38394a80c8e6b8e04c46473e0e824f
azarrias <adolfo.zarrias@gmail.com>
2020-11-13 18:50:57 +0100

    Make laser beam fragment shader and add it to the play scene

--- src/ScenePlay.lua
+++ src/ScenePlay.lua
@@ -5,6 +5,10 @@ function ScenePlay:init()
   self.bgSky = SkyLayer(20)
   self.mgSky = SkyLayer(25)
   self.fgSky = SkyLayer(30)
+  
+  self.shader = love.graphics.newShader("shaders/laser_beam.fs")
+  self.shader:send("resolution", { VIRTUAL_SIZE.x, VIRTUAL_SIZE.y })
+  self.start_time = love.timer.getTime()
 end
 
 function ScenePlay:update(dt)
@@ -12,6 +16,8 @@ function ScenePlay:update(dt)
   self.mgSky:update(dt)
   self.fgSky:update(dt)
   self.player:update(dt)
+  self.shader:send("time", love.timer.getTime() - self.start_time)
+  self.shader:send("position", { self.player.position.x + 24, self.player.position.y + 1 })
 end
 
 function ScenePlay:render()
@@ -19,6 +25,10 @@ function ScenePlay:render()
   self.mgSky:render()
   self.fgSky:render()
   self.player:render()
+  love.graphics.setShader(self.shader)
+  love.graphics.setColor(1, 0, 0, 0)
+  love.graphics.rectangle('fill', 0, 0, VIRTUAL_SIZE.x * 5, VIRTUAL_SIZE.y)
+  love.graphics.setShader()
 end
 
 function ScenePlay:CreatePlayer()
---
+++ src/shaders/laser_beam.fs
@@ -0,0 +1,37 @@
+#define PI 3.14159265358979323846
+extern vec2 resolution;
+extern float time;
+extern vec2 position;
+
+float Circle(vec2 uv, vec2 pos, float radius, float step_size)
+{
+  uv.y *= 2;
+  float d = length(uv - pos);                           // distance from every pixel to pos
+  float c = radius / d;
+  //float c = smoothstep(radius, radius - step_size, d);  // smooth edge of the circle (reversed to go from high to low!)
+  
+  return c;
+}
+
+vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
+{
+  vec2 uv = vec2(screen_coords.x / resolution.x, screen_coords.y / resolution.y); // normalize coords
+  float aspect_ratio = resolution.x / resolution.y;
+  uv.x *= aspect_ratio;  // multiply x by aspect ratio to compensate for the width vs height difference
+  
+  uv.y -= position.y / resolution.y;
+  uv.x -= position.x / resolution.x * aspect_ratio;
+  
+  vec4 col = vec4(0.);
+  vec4 beam_color = vec4(1., 1., 0., 1.);
+  
+  float c = Circle(uv, vec2(0.), .025, 0.01);
+  col += c * beam_color;
+  
+  float beam = max(0, 1 - abs(uv.y * 75));
+  if (uv.x < 0 || time * 5 - uv.x < 0) beam = 0;
+  
+  col += beam * beam_color;
+  
+  return vec4(col);              // use the same c value for r, g and b
+}
